c378dc7a08270609e905b5a05d68f79c10ca3f80
Serena Savarese <104151406+babysauro@users.noreply.github.com>
2023-11-14 16:51:52 +0100

    Uploaded NC1 app

---
+++ Spotify/Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Serena Savarese on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
